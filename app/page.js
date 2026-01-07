c39ffb1f69c7510a42a36559621a604674020faf
PRAGADEESH G <150640941+PRAGADEESH2903@users.noreply.github.com>
2026-01-07 13:33:34 +0530

    page.js

--- app/page.js
+++ app/page.js
@@ -1,7 +1,8 @@
 "use client";
 
 import { useState } from "react";
-import { supabase } from "../lib/supabaseClient";
+import { createClient } from "@supabase/supabase-js";
+
 
 
 export default function Home() {
